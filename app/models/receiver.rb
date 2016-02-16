b3880b7f5f6704c258b5ad34e89b81c9dcb30f15
Yang Liu <jeremyrobturtle@gmail.com>
2016-02-15 22:01:43 -0500

    send to slack now in Receiver model

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -1,2 +1,12 @@
 class Receiver < ActiveRecord::Base
+  def send_to_slack json
+    uri = URI.parse(receiver.url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if URI::HTTPS === uri
+
+    request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+    request.body = json.to_json
+
+    http.request(request)
+  end
 end
